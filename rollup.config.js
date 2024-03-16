55097072e95e3d4dc47af4ee032b5931822f427b
XL-YiBai <lei.xia@emqx.io>
2024-03-16 16:09:03 +0800

    feat: init project

---
+++ rollup.config.js
@@ -0,0 +1,34 @@
+import resolve from '@rollup/plugin-node-resolve'
+import commonjs from '@rollup/plugin-commonjs'
+import typescript from '@rollup/plugin-typescript'
+
+export default [
+  {
+    // 入口
+    input: 'packages/vue/src/index.ts',
+    output: [
+      // 导出 iife 模式的包
+      {
+        // 开启 sourceMap
+        sourcemap: true,
+        // 导出文件地址
+        file: './packages/vue/dist/vue.js',
+        // 生成包的格式
+        format: 'iife',
+        // 变量名
+        name: 'Vue'
+      }
+    ],
+    // 插件
+    plugins: [
+      // ts
+      typescript({
+        sourceMap: true
+      }),
+      // 模块导入的路径补全
+      resolve(),
+      // 转 commonjs 为 ESM
+      commonjs()
+    ]
+  }
+]
