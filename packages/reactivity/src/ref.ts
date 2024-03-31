0ac026c4e3acb81d66b3e8cd576bcf3caa8f5db6
XL-YiBai <lei.xia@emqx.io>
2024-03-31 22:27:05 +0800

    feat(reactivity): improve ref function

--- packages/reactivity/src/ref.ts
+++ packages/reactivity/src/ref.ts
@@ -1,6 +1,7 @@
 import { Dep, createDep } from './dep'
 import { toReactive } from './reactive'
-import { activeEffect, trackEffects } from './effect'
+import { activeEffect, trackEffects, triggerEffects } from './effect'
+import { hasChanged } from '@vue/shared'
 
 export interface Ref<T = any> {
   value: T
@@ -20,26 +21,48 @@ function createRef(rawValue: unknown, shallow: boolean) {
 
 class RefImpl<T> {
   private _value: T
+  private _rawValue: T
 
   private dep?: Dep = undefined
 
   public readonly __v_isRef = true
 
   constructor(value: T, public readonly __v_isShallow: boolean) {
+    this._rawValue = value
     this._value = __v_isShallow ? value : toReactive(value)
   }
 
   get value() {
-    this
+    trackRefValue(this)
     return this._value
   }
 
-  set value(newValue) {}
+  set value(newVal) {
+    if (hasChanged(newVal, this._rawValue)) {
+      this._rawValue = newVal
+      this._value = toReactive(newVal)
+      triggerRefValue(this)
+    }
+  }
 }
 
+/**
+ * 收集依赖
+ * @param ref RefImpl
+ */
 export function trackRefValue(ref) {
   if (activeEffect) {
     trackEffects(ref.dep || (ref.dep = createDep()))
+  }
+}
+
+/**
+ * 触发依赖
+ * @param ref RefImpl
+ */
+export function triggerRefValue(ref) {
+  if (ref.dep) {
+    triggerEffects(ref.dep)
   }
 }
 
--- packages/shared/src/index.ts
+++ packages/shared/src/index.ts
@@ -10,3 +10,12 @@ export const isArray = Array.isArray
  */
 export const isObject = (value: unknown) =>
   value !== null && typeof value === 'object'
+
+/**
+ * 对比两个数据是否发生改变
+ * @param value
+ * @param oldValue
+ * @returns
+ */
+export const hasChanged = (value: any, oldValue: any): boolean =>
+  !Object.is(value, oldValue)
