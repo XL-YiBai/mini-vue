099e5eebbf2e9cd14f5b7040b1ac6b2588731a99
XL-YiBai <lei.xia@emqx.io>
2024-05-20 23:06:53 +0800

    feat(runtime): render component (data)

--- packages/runtime-core/src/component.ts
+++ packages/runtime-core/src/component.ts
@@ -1,3 +1,6 @@
+import { reactive } from '@vue/reactivity'
+import { isObject } from '@vue/shared'
+
 let uid = 0
 
 export function createComponentInstance(vnode) {
@@ -28,4 +31,19 @@ export function finishComponentSetup(instance) {
 
   // 把组件上的 render 方法赋值到 instance 实例上
   instance.render = Component.render
+
+  applyOptions(instance)
+}
+
+function applyOptions(instance: any) {
+  // 这里解构出来的 dataOptions 其实就是 component 中那个 data() { return { msg: 'xx', ... } }
+  const { data: dataOptions } = instance.type
+
+  if (dataOptions) {
+    const data = dataOptions()
+    if (isObject(data)) {
+      // 把 data 转换成响应性数据，挂载到 instance.data
+      instance.data = reactive(data)
+    }
+  }
 }
--- packages/runtime-core/src/componentRenderUtils.ts
+++ packages/runtime-core/src/componentRenderUtils.ts
@@ -2,13 +2,14 @@ import { ShapeFlags } from 'packages/shared/src/shapeFlags'
 import { Text, createVNode } from './vnode'
 
 export function renderComponentRoot(instance) {
-  const { vnode, render } = instance
+  const { vnode, render, data } = instance
 
   let result
 
   try {
     if (vnode.shapeFlag & ShapeFlags.STATEFUL_COMPONENT) {
-      result = normalizeVNode(render!())
+      // 通过 call 改变 this 指向，这样在 render 中通过 this.xxx 就能拿到 data 中的数据
+      result = normalizeVNode(render!.call(data))
     }
   } catch (error) {
     console.error(error)
