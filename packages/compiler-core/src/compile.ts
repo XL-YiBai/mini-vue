dc1adb14421d4ea7e690008cb1398af3f4a6d5b5
XL-YiBai <lei.xia@emqx.io>
2024-08-03 00:16:34 +0800

    feat(compiler): v-if transform

--- packages/compiler-core/src/compile.ts
+++ packages/compiler-core/src/compile.ts
@@ -4,18 +4,19 @@ import { transform } from './transform'
 import { transformElement } from './transforms/transformElement'
 import { transformText } from './transforms/transformText'
 import { generate } from './codegen'
+import { transformIf } from './transforms/vif'
 
 export function baseCompile(template: string, options = {}) {
   const ast = baseParse(template)
-  console.log('ast', JSON.stringify(ast))
+  // console.log('ast', JSON.stringify(ast))
 
   transform(
     ast,
     extend(options, {
-      nodeTransforms: [transformElement, transformText]
+      nodeTransforms: [transformElement, transformText, transformIf]
     })
   )
 
-  // console.log(ast)
+  console.log(ast)
   return generate(ast)
 }
